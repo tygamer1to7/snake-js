5ad9f8a8689673151ed8df0dacce499a06e5fb62
Joshua Kravitz <jlkravitz@gmail.com>
2013-05-19 07:57:32 -0700

    edited bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,7 @@
 		var snakeJS = document.createElement('script');
 		snakeJS.src = 'https://raw.github.com/jlkravitz/snake-js/master/snake.js';
 		snakeJS.onload = function() { Snake.run() }; 
-		document.body.appendChild(snakeJS)
+		document.body.appendChild(snakeJS);
 	}
 
 	if(!($ = window.jQuery)) {
@@ -11,5 +11,7 @@
 		jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
 		jquery.onload = runSnake;
 		document.body.appendChild(jquery);
-	} else { runSnake() }
+	} else { 
+		runSnake();
+	}
 })()

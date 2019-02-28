7eb8da086b014fdd8326ac2b02246e5e329030a0
tygamer1to7 <11811416+tygamer1to7@users.noreply.github.com>
2019-02-28 14:04:50 -0600

    hmm

--- bookmarklet-minv2.js
+++ bookmarklet-minv2.js
@@ -1,17 +1,1 @@
-function() {
-	function runSnake() {
-		var snakeJS = document.createElement('script');
-		snakeJS.src = 'https://rawgit.com/jlkravitz/snake-js/master/snake.js';
-		snakeJS.onload = function() { Snake.run() }; 
-		document.body.appendChild(snakeJS);
-	}
-
-	if(!($ = window.jQuery)) {
-		var jquery = document.createElement('script');
-		jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-		jquery.onload = runSnake;
-		document.body.appendChild(jquery);
-	} else { 
-		runSnake();
-	}
-)()
+function() {function runSnake() {var snakeJS = document.createElement('script');snakeJS.src = 'https://rawgit.com/jlkravitz/snake-js/master/snake.js';snakeJS.onload = function() { Snake.run() }; document.body.appendChild(snakeJS);}if(!($ = window.jQuery)) {var jquery = document.createElement('script');jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';jquery.onload = runSnake;document.body.appendChild(jquery);} else { runSnake();})()
